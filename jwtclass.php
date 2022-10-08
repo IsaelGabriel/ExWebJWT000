ffc6e31b52dc18cfab96a3489474f54047a8ef42
IsaelGabriel <isaelgabrielfaria@gmail.com>
2022-10-07 22:55:07 -0300

    update

--- jwtclass.php
+++ jwtclass.php
@@ -1,7 +1,8 @@
 <?php
     class myJWT
     {
-        private $password = "passwordSecreta";
+        private $password = "senhaSecreta";
+
         public function generate_token($payload)
         {
             $header = [
@@ -30,13 +31,90 @@
         
             $signatureCheck = hash_hmac('sha256',"$header.$payload",$this->password,true);
             $signatureCheck = base64_encode($signatureCheck);
-            if ($signature == $signatureCheck){
-            $result = true;
-            }else {
-            $result = false;
+            if ($signature == $signatureCheck)
+            {
+                $result = true;
+            }
+            else
+            {
+                $result = false;
             }
         
             return $result;
+        }
+
+        public function get_payload($token)
+        {
+            $part = explode(".",$token);
+            $header = $part[0];
+            $payload = $part[1];
+            $signature = $part[2];
+
+            $decoded_payload = base64_decode($payload);
+            return $decoded_payload;
+        }
+    }
+
+    class myRefreshToken
+    {
+        private $password = "senhaSecreta";
+
+        public function generate_refresh_token($user)
+        {
+            $header = [
+                'alg' => 'HS256',
+                'typ' => 'JWT'
+            ];
+            
+            $header = json_encode($header);
+            $header = base64_encode($header);
+            
+            $payload = [
+                'issued_at' => time(),
+                'user' => $user
+            ];
+
+            $payload = json_encode($payload);
+            $payload = base64_encode($payload);
+        
+            $signature = hash_hmac('sha256',"$header.$payload",$this->password,true);
+            $signature = base64_encode($signature);
+        
+            return "$header.$payload.$signature";
+        }
+
+        public function set_jwt($jwt,$rt)
+        {
+            $part = explode(".",$jwt);
+            $header = $part[0];
+            $payload = $part[1];
+            $signature = $part[2];
+
+            $decoded_payload = (array)base64_decode($payload);
+
+            if($decoded_payload['exp'] <= time() && $decoded_payload['rt'] == $rt)
+            {
+                $decoded_payload['exp'] = time() + 120;
+                $payload = $decoded_payload;
+                $payload = json_encode($payload);
+                $payload = base64_encode($payload);
+                return $header.$payload.$signature;
+            }
+
+            return $jwt;
+        }
+
+        public function validate_token($jwt,$rt)
+        {
+            $payload = explode(".",$jwt)[1];
+
+            $decoded_payload = (array)base64_decode($payload);
+            if($decoded_payload['rt'] == $rt)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 ?>
--- receive_form.php
+++ receive_form.php
@@ -4,12 +4,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body {
+            text-align: center;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translateX(-50%) translateY(-50%);
+            inline-size: 80%;
+            overflow-wrap: break-word;
+        }
+
+    </style>
 </head>
 <body>
     <?php
         require 'jwtclass.php';
-
         $myjwt = new myJWT();
+        $myrt = new myRefreshToken();
+
         $user = "adminprogweb";
         $pass = "ProgWeb3";
         $db = "progweb3";
@@ -17,44 +30,66 @@
         if ($conn->connect_errno)
         {
             die("<h1> Erro de conexão:" . $conn->connect_error . "</h1>");
-        } 
+        }
         
         $user_id = $_POST["usuario"];
         $user_password = $_POST["senha"];
-        $sql = "select * from usuarios where idusuario = '". $user_id ."' and senhausuario = '". $user_password ."'";
-        $resultadoQuery = mysqli_query($conn, $sql);
-        if ($resultadoQuery->num_rows == 0 ){
-            die("Usuário ou senha inválidos");
+        $sql = "select * from usuarios where idusuario = '{$user_id}' and senhausuario = '{$user_password}'";
+        $query_result = mysqli_query($conn, $sql);
+        if ($query_result->num_rows == 0 ){
+            die("<h1>Usuário e/ou senha inválidos!</h1>");
         }
-        $arrayQuery = $resultadoQuery->fetch_assoc();
-        echo "</br>";
-        echo "usuário digitado: " . $arrayQuery["idusuario"];
-        echo "</br></br>";
-        echo "</br>";
-        echo "senha digitada: " . $arrayQuery["senhausuario"];
+        $array_query = $query_result->fetch_assoc();
         
-        $payload = [
-            'iss' => 'localhost',
-            'nome' => $arrayQuery["nomeusuario"],
-            'email' => $arrayQuery["email"]
-        ];
-        
-        echo "</br>";
-        echo "</br>";
-        $token = $myjwt->generate_token($payload);
-        echo $token;
-        
-        echo "</br>";
-        echo "</br>";
-        echo "Token validado com sucesso?</br>";
-        if ($myjwt->validate_token($token)){
-            echo "sim</br>";
-        }else{
-            echo "não</br>";
+        if (isset($_POST["token"]))
+        {
+            echo "</br> Usuário digitado: {$array_query["idusuario"]} </br></br>
+                </br>senha digitada:  {$array_query["senhausuario"]}</br></br>";
+
+            $token = $_POST["token"];
+            $refresh_token = $_POST["refresh_token"];
+            if ($myrt->validate_token($token,$refresh_token))
+            {
+                $payload = $myjwt->get_payload($token);
+                $payload = (array)json_decode($payload);
+                $token = $myrt->set_jwt($token,$refresh_token);
+                $remaining_time = $payload["exp"] - time();
+                echo "Token possui {$remaining_time} segundos restantes.";
+            }else{
+                die("Refresh token inválido!<br><br>");
+            }
+
         }
-        
-        
-        
+        else
+        {
+            $refresh_token = $myrt->generate_refresh_token($user_id);
+            $payload = [
+                'iss' => 'localhost',
+                'nome' => $array_query["nomeusuario"],
+                'email' => $array_query["email"],
+                'exp' => time() + 120, //expira em 2 minutos
+                'rt' => $refresh_token
+            ];
+            $token = $myjwt->generate_token($payload);
+            
+        }
+
+        echo "</br> Usuário digitado: {$array_query["idusuario"]} </br></br>
+                </br>senha digitada:  {$array_query["senhausuario"]}
+                </br></br></br>Token:</br>{$token}
+                </br></br>Token validado com sucesso: ";
+            
+            if ($myjwt->validate_token($token)){
+                echo "Sim</br>";
+            }else{
+                echo "Não</br>";
+            }
     ?>
+    <form action="./receive_form.php" method="post">
+    <input type="hidden" value=<?php echo $user_id ?> name="usuario">
+    <input type="hidden" value=<?php echo $user_password ?> name="senha">
+    <input type="hidden" value=<?php echo $token ?> name="token">
+    <input type="hidden" value=<?php echo $refresh_token ?> name="refresh_token">
+    </br><input type="submit" value="Refresh"></form>
 </body>
 </html>
